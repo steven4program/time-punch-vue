5b8901e639d5dde9e675fe0765465636cb4f770d
steven4program <stevenchang421@gmail.com>
2023-01-01 22:06:01 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
 
 const app = createApp(App)
 
